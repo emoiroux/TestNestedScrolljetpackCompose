1081679a42c93e85c2f5d2be9f1f42b3ec2b10d3
etienne moiroux <emoiroux@ibm.com>
2023-09-19 08:50:00 -0300

    cleanup

--- app/src/main/java/com/example/testnestedscroll/TestComposeNestedScroll.kt
+++ app/src/main/java/com/example/testnestedscroll/TestComposeNestedScroll.kt
@@ -23,14 +23,12 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -39,7 +37,6 @@ import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
@@ -47,40 +44,32 @@ import kotlin.math.roundToInt
 import androidx.compose.material.TopAppBar
 
 
-class FirstFragmentPureCompose : Fragment() {
+class TestComposeNestedScroll : Fragment() {
 
-    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                Test()
+                Option1NestedScrollModifier()
             }
         }
     }
 
-    @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
     @Composable
-    private fun Test() {
+    private fun Option2CustomPager() {
+        // TODO
+    }
+    @Composable
+    private fun Option1NestedScrollModifier() {
 
-// here we use LazyColumn that has build-in nested scroll, but we want to act like a
-// parent for this LazyColumn and participate in its nested scroll.
-// Let's make a collapsing toolbar for LazyColumn
         val toolbarHeight = 48.dp
-        val toolbarHeightPx = with(LocalDensity.current) { toolbarHeight.roundToPx().toFloat() }
-// our offset to collapse toolbar
         val toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
-// now, let's create connection to the nested scroll system and listen to the scroll
-// happening inside child LazyColumn
+
         val nestedScrollConnection = remember {
             object : NestedScrollConnection {
                 override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                    // try to consume before LazyColumn to collapse toolbar if needed, hence pre-scroll
                     val delta = available.y
                     val newOffset = toolbarOffsetHeightPx.value + delta
                     toolbarOffsetHeightPx.value = newOffset//.coerceIn(-toolbarHeightPx, 0f)
-                    // here's the catch: let's pretend we consumed 0 in any case, since we want
-                    // LazyColumn to scroll anyway for good UX
-                    // We're basically watching scroll without taking it
                     return Offset.Zero
                 }
 
@@ -88,9 +77,7 @@ class FirstFragmentPureCompose : Fragment() {
         }
         Box(Modifier
             .fillMaxSize()
-            // attach as a parent to the nested scroll system
             .nestedScroll(nestedScrollConnection)) {
-            // our list with build in nested scroll support that will notify us about its scroll
             LazyColumn(contentPadding = PaddingValues(top = toolbarHeight)) {
                 items(100) { index ->
                     Text("I'm item $index", modifier = Modifier
@@ -107,70 +94,7 @@ class FirstFragmentPureCompose : Fragment() {
 
     @OptIn(ExperimentalFoundationApi::class)
     @Composable
-    private fun NestedScroll() {
-        val rememberScrollState = rememberScrollState()
-        val screenHeaderHeight = LocalConfiguration.current.screenHeightDp.dp
-        val pagerState = rememberPagerState()
-
-        Column(modifier = Modifier
-            .verticalScroll(state = rememberScrollState)
-            .fillMaxHeight()) {
-
-            var delta by remember { mutableStateOf(0f) }
-
-            val nestedScrollConnection = remember {
-                object : NestedScrollConnection {
-                    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                        delta -= available.y
-                        Log.e("onPreScroll", "delta : $delta")
-                        return Offset.Zero
-                    }
-                }
-            }
-
-
-            Box(modifier = Modifier
-                .background(Color.Black)
-                .height(200.dp)
-
-                .fillMaxWidth()
-                .offset(y = delta.dp)) {
-                Text("Header")
-            }
-
-            HorizontalPager(pageCount = 3, state = pagerState) {
-                val lazyListState = rememberLazyListState()
-
-
-                LazyColumn(
-                    state = lazyListState,
-                    userScrollEnabled = true,
-                    modifier = Modifier
-                        .height(screenHeaderHeight)
-                        .nestedScroll(nestedScrollConnection),
-                    /*state = scrollState*/
-                ) {
-
-                    items(100) {
-
-                        Log.e("item", "item : $it")
-                        Box(modifier = Modifier
-                            .background(color = Color.LightGray)
-                            .height(200.dp)
-                            .fillMaxWidth()) {
-
-                            Text("Item $it / 100", modifier = Modifier.height(100.dp))
-                        }
-                        Spacer(modifier = Modifier.height(20.dp))
-                    }
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalFoundationApi::class)
-    @Composable
-    private fun UserScrollEnabled() {
+    private fun Option3UserScrollEnabled() {
         val rememberScrollState = rememberScrollState()
         val screenHeaderHeight = LocalConfiguration.current.screenHeightDp.dp
         val pagerState = rememberPagerState()
@@ -221,13 +145,7 @@ class FirstFragmentPureCompose : Fragment() {
                         Spacer(modifier = Modifier.height(20.dp))
                     }
                 }
-
             }
-
-            //  snapshotFlow { endReached }.collect {
-            //    Log.e("snapshotFlow", it.toString())
-            // }
-
         }
     }
 }
